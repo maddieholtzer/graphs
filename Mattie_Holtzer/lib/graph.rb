453db8fa7b4a508bf88ef2ee9a6928183f68dcda
Mattie <mattieholtzer@gmail.com>
2018-05-08 07:36:13 -0700

    edge init done

--- Mattie_Holtzer/lib/graph.rb
+++ Mattie_Holtzer/lib/graph.rb
@@ -7,14 +7,27 @@ class Vertex
     @in_edges = []
     @out_edges = []
   end
+
+  def add_in(edge)
+    @in_edges.push(edge)
+  end
+
+  def add_out(edge)
+    @out_edges.push(edge)
+  end
+
 end
 
 class Edge
 
-  attr_reader :in_edges
+  attr_reader :from_vertex, :to_vertex, :cost
 
   def initialize(from_vertex, to_vertex, cost = 1)
-    @in_edges = []
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    @to_vertex.add_in(self)
+    @from_vertex.add_out(self)
   end
 
   def destroy!
