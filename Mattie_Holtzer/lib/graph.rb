43d9ddda8d0f6fa0332ee28b47bece3bf338b33a
Mattie <mattieholtzer@gmail.com>
2018-05-08 07:29:04 -0700

    vertex done

--- Mattie_Holtzer/lib/graph.rb
+++ Mattie_Holtzer/lib/graph.rb
@@ -1,15 +1,20 @@
 class Vertex
 
-  attr_reader :value
+  attr_reader :value, :in_edges, :out_edges
 
   def initialize(value)
     @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
 
+  attr_reader :in_edges
+
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @in_edges = []
   end
 
   def destroy!
